f4e2c39f0ee0265514bbc83cefa9f5f8cf2bf41d
rolara57 <roger@qm46.com>
2021-01-04 20:50:48 -0600

    updated list

--- db/seed.sql
+++ db/seed.sql
@@ -23,11 +23,11 @@ VALUES
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('John', 'McEnroe', 1, NULL),
+    ('Charlie', 'Chan', 2, 1),
+    ('Alex', 'Rodriguez', 3, NULL),
+    ('Tupac', 'Shakur', 4, 3),
+    ('Atilla', 'Hun', 5, NULL),
+    ('Chuck', 'Brown', 6, 5),
+    ('Obiwan', 'Kenobi', 7, NULL),
+    ('Tim', 'Allen', 8, 7);
